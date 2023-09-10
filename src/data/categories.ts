ad2b2fd597faaaf3aa75eaca91177609cc300fa4
Kanahiro <mediterranean1769@gmail.com>
2023-09-10 16:56:15 +0900

    init

---
+++ src/data/categories.ts
@@ -0,0 +1,2 @@
+// List of categories for blog posts
+export const CATEGORIES = ['News', 'Technique'] as const;
---
+++ src/data/site.config.ts
@@ -0,0 +1,17 @@
+interface SiteConfig {
+    author: string;
+    title: string;
+    description: string;
+    lang: string;
+    ogLocale: string;
+    shareMessage: string;
+}
+
+export const siteConfig: SiteConfig = {
+    author: 'MapLibre User Group Japan',
+    title: 'MapLibre User Group Japan',
+    description: 'The official website of MapLibre User Group Japan',
+    lang: 'ja-JP',
+    ogLocale: 'ja_JP',
+    shareMessage: 'MapLibre User Group Japan', // Message to share a post on social media
+};
